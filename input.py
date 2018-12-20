387c150a31197dfc1be4eb08f87ba3345bf2deb7
calvinb4 <calvinbertoncini@gmail.com>
2018-12-20 11:04:02 -0500

    12-20-18

--- input.py
+++ input.py
@@ -10,9 +10,11 @@ class Window(QWidget):
 
     def initUI(self):
 
+        self.lbl2 = QLabel(self)
+        
         self.btn = QPushButton("Ok!", self)
         self.btn.move(100, 50)
-        self.btn.clicked.connect(self.showDialog)
+        self.btn.clicked.connect(self.clicked)
 
         self.le = QLineEdit(self)
         self.le.move(100, 20)
@@ -24,12 +26,15 @@ class Window(QWidget):
         self.setWindowTitle("Window")
         self.show()
 
-    def showDialog(self):
-
-        text, ok = QInputDialog.getText(self, "Answer", "Hi")
-
-        if ok:
-            self.le.setText(str(text))
+    def clicked(self):
+        if (self.le.text() == "jisho"):
+            self.lbl2.setText("Correct")
+            self.lbl2.adjustSize()
+            self.lbl2.move(200, 200)
+        else:
+            self.lbl2.setText("Incorrect")
+            self.lbl2.adjustSize()
+            self.lbl2.move(200, 200)
 
 if __name__ == "__main__":
 
