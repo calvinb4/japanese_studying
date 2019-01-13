1bc015c0233d917a4891b6e6c413aa364f04c6e3
calvinb4 <calvinbertoncini@gmail.com>
2019-01-13 17:42:27 -0500

    1-13-18

--- input.py
+++ input.py
@@ -1,5 +1,8 @@
 from PyQt5.QtWidgets import QWidget, QPushButton, QLineEdit, QInputDialog, QApplication, QLabel
 import sys
+import random
+
+a = {"dictionary":"jisho","you":"anata","to hear":"kiku","stairs":"kaidan","white":"shiro"}
 
 class Window(QWidget):
 
@@ -10,6 +13,10 @@ class Window(QWidget):
 
     def initUI(self):
 
+        self.count = 0
+
+        self.setup_dictionary()
+        
         self.lbl2 = QLabel(self)
         
         self.btn = QPushButton("Ok!", self)
@@ -18,8 +25,8 @@ class Window(QWidget):
 
         self.le = QLineEdit(self)
         self.le.move(100, 20)
-
-        self.lbl1 = QLabel("Answer", self)
+        
+        self.lbl1 = QLabel(self.keys[self.count], self)
         self.lbl1.move(300, 300)
 
         self.setGeometry(100, 100, 1000, 500)
@@ -27,14 +34,25 @@ class Window(QWidget):
         self.show()
 
     def clicked(self):
-        if (self.le.text() == "jisho"):
+        key = self.keys[self.count]
+        
+        if (self.le.text() == a[key]):
             self.lbl2.setText("Correct")
             self.lbl2.adjustSize()
             self.lbl2.move(200, 200)
+            self.count += 1
+            self.lbl1 = QLabel(self.keys[self.count], self)
+            self.lbl1.move(300, 300)
+            
         else:
             self.lbl2.setText("Incorrect")
             self.lbl2.adjustSize()
             self.lbl2.move(200, 200)
+
+    def setup_dictionary(self):
+        self.keys = list(a.keys())
+        random.shuffle(self.keys)
+        
 
 if __name__ == "__main__":
 
